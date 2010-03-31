3908c0edb22076c7d57d67231cb68eb6b42639eb
Christophe Varoqui <christophe.varoqui@free.fr>
2010-03-31 15:00:51 +0200

    Ajax filter value cloud-completion

--- init/controllers/default.py
+++ init/controllers/default.py
@@ -1640,6 +1640,35 @@ def obsolescence_config():
                 available_filters=avail_db_filters('v_nodes'),
                 nav=nav)
 
+def ajax_filter_cloud():
+    val = request.vars.filtervalue
+    fil = request.vars.addfilter
+    filters = db(db.filters.id==fil).select()
+    if len(filters) == 0:
+        return DIV()
+    if filters[0].fil_need_value != 1:
+        return DIV()
+    n = {}
+    for f in filters:
+        rows = db(db[f.fil_table][f.fil_column].like('%'+val+'%')).select(db[f.fil_table][f.fil_column])
+        for i in [r[f.fil_column] for r in rows]:
+            if i in n:
+                n[i] += 1
+            else:
+                n[i] = 1
+    if len(n) == 0:
+        return DIV()
+    c_max = max(n.values())
+    def format_item(i, c, c_max):
+        s = float(c) / c_max * 100 + 50
+        return SPAN(i+' ', _style='font-size:'+str(s)+'%;padding:0.5em')
+    d = []
+    for i in sorted(n):
+        if i == '':
+            continue
+        d += [format_item(i, n[i], c_max)]
+    return SPAN(d)
+
 def ajax_obsolete_os_nodes():
     if request.vars.obs_type == "os":
         query = (db.obsolescence.obs_type=="os")&(db.v_nodes.os_concat==request.vars.obs_name)
