781ee0ec745187816e6bede2ee8239bb9a561730
Christophe Varoqui <christophe.varoqui@free.fr>
2010-05-16 12:09:59 +0200

    Display detailled package differences

--- init/controllers/default.py
+++ init/controllers/default.py
@@ -1490,6 +1490,45 @@ def svcmon_log():
                )
 
 @auth.requires_login()
+def ajax_pkgdiff():
+    nodes = request.vars.pkgnodes.split(',')
+    n = len(nodes)
+    sql = """select * from (
+               select *,count(pkg_nodename) as c
+               from packages
+               where pkg_nodename in (%(nodes)s)
+               group by pkg_name,pkg_version,pkg_arch
+               order by pkg_name,pkg_version,pkg_arch
+             ) as t
+             where t.c!=%(n)s;
+          """%dict(n=n, nodes=','.join(map(repr, nodes)))
+    rows = db.executesql(sql)
+
+    def fmt_header():
+        return TR(
+                 TH(T("Node")),
+                 TH(T("Package")),
+                 TH(T("Version")),
+                 TH(T("Arch")),
+               )
+
+    def fmt_line(row):
+        return TR(
+                 TD(row[1]),
+                 TD(row[2]),
+                 TD(row[3]),
+                 TD(row[4]),
+               )
+
+    def fmt_table(rows):
+        return TABLE(
+                 fmt_header(),
+                 map(fmt_line, rows),
+               )
+
+    return DIV(fmt_table(rows))
+
+@auth.requires_login()
 def ajax_envfile():
     return DIV(
              H3(T("Service configuration file for %(svc)s",dict(
