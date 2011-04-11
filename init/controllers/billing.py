d2b38939c01c97518652e66d98167c3c04550d55
Christophe Varoqui <christophe.varoqui@opensvc.com>
2011-04-11 15:03:00 +0200

    Order billing

--- init/controllers/billing.py
+++ init/controllers/billing.py
@@ -1,9 +1,9 @@
 @auth.requires_membership('Manager')
 def billing():
     query = (db.v_billing_per_os.nb!=0)
-    billing_per_os = db(query).select()
+    billing_per_os = db(query).select(orderby=~db.v_billing_per_os.nb)
     query = (db.v_billing_per_app.nb!=0)
-    billing_per_app = db(query).select()
+    billing_per_app = db(query).select(orderby=~db.v_billing_per_app.nb)
     return dict(billing_per_os=billing_per_os, billing_per_app=billing_per_app)
 
 
