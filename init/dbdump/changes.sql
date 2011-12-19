d96944b40314d6af3c66ed749b5ebe1794f8c67f
Christophe Varoqui <christophe.varoqui@free.fr>
2011-12-19 17:40:21 +0100

    Reset v_comp_nodes view
    since we added team_integ and project

--- init/dbdump/changes.sql
+++ init/dbdump/changes.sql
@@ -1877,3 +1877,8 @@ CREATE VIEW `v_svcactions` AS select `ac`.`cron` AS `cron`,`ac`.`time` AS `time`
 
 alter table stats_netdev_err add key stats_netdev_err_k1 (nodename);
 alter table stats_netdev_err add key stats_netdev_err_k2 (date);
+
+drop view v_comp_nodes;
+
+create view v_comp_nodes as (select n.*,group_concat(distinct r.ruleset_name separator ', ') as rulesets, group_concat(distinct m.modset_name separator ', ') as modulesets from v_nodes n left join comp_rulesets_nodes rn on n.nodename=rn.nodename left join comp_rulesets r on r.id=rn.ruleset_id left join comp_node_moduleset mn on mn.modset_node=n.nodename left join comp_moduleset m on m.id=mn.modset_id group by n.nodename);
+
