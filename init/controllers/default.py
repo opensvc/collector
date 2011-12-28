a53f5db0e4a1eedbadf3641a9c40f420ff79d83c
Christophe Varoqui <christophe.varoqui@opensvc.com>
2011-12-28 12:50:40 +0100

    Deduplicate disk groups of local disks in topo

--- init/controllers/default.py
+++ init/controllers/default.py
@@ -298,7 +298,10 @@ class viz(object):
             </table>>]"""%dict(dg=dg, cdg=cdg, n=''.join(self.cdg[cdg]))
 
     def vid_cdg(self, d):
-        key = d.disk_arrayid,d.disk_svcname,d.disk_dg
+        if d.disk_id.startswith(d.disk_nodename):
+            key = d.disk_arrayid,d.disk_nodename,d.disk_dg
+        else:
+            key = d.disk_arrayid,d.disk_svcname,d.disk_dg
         cdg = 'cdg_'+str(len(self.vidcdg))
         if key not in self.vidcdg:
             self.vidcdg[key] = cdg
