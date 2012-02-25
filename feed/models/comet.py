d1f692735e3cc4a4249ac6bcf07ddfcbbdc263e8
Christophe Varoqui <christophe.varoqui@opensvc.com>
2012-02-25 21:20:45 +0100

    ws to wss

--- feed/models/comet.py
+++ feed/models/comet.py
@@ -1,6 +1,6 @@
 from gluon.contrib.comet_messaging import comet_send
 
-comet_url = "http://dbopensvc:8889"
+comet_url = "https://dbopensvc:8889"
 comet_key = "magix123"
 
 def _comet_send(msg, group="generic"):
