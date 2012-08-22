88737795b5825c873b666df959bf29a51f4b18d0
Christophe Varoqui <christophe.varoqui@free.fr>
2012-08-22 15:44:53 +0200

    Adapt chk_inst string length in web2py form to db field size

--- init/models/db.py
+++ init/models/db.py
@@ -873,7 +873,7 @@ db.define_table('checks_live',
 
 db.define_table('checks_defaults',
     Field('chk_type', 'string', length=10, writable=False),
-    Field('chk_inst', 'string', length=10),
+    Field('chk_inst', 'string', length=128),
     Field('chk_low', 'integer'),
     Field('chk_high', 'integer'),
     migrate=False)
