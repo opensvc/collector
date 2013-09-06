f1a5b69cf2f99c61b5cd7ab6479be7083bf4effa
Christophe Varoqui <christophe.varoqui@free.fr>
2013-09-06 16:50:11 +0200

    Purge alerts with no node info

--- init/controllers/cron.py
+++ init/controllers/cron.py
@@ -821,17 +821,8 @@ def update_dg_quota():
 
 def purge_alerts_on_nodes_without_asset():
     l = db.nodes.on(db.dashboard.dash_nodename==db.nodes.nodename)
-    q = db.dashboard.dash_type == "node information not updated"
+    q = db.dashboard.id > 0
     q &= db.nodes.nodename == None
-    ids = map(lambda x: x.id, db(q).select(db.dashboard.id, left=l))
-    if len(ids) > 0:
-        q = db.dashboard.id.belongs(ids)
-        db(q).delete()
-        db.commit()
-
-    l = db.svcmon.on(db.dashboard.dash_nodename==db.svcmon.mon_nodname)
-    q = db.dashboard.dash_type == "node without asset information"
-    q &= db.svcmon.mon_nodname == None
     ids = map(lambda x: x.id, db(q).select(db.dashboard.id, left=l))
     if len(ids) > 0:
         q = db.dashboard.id.belongs(ids)
