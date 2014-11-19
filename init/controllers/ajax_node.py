99550cf0c4ee57a1212e726922c3785c34c497e1
Christophe Varoqui <christophe.varoqui@free.fr>
2014-11-19 12:19:37 +0100

    Add sec_zone to node info in extraline tab

--- init/controllers/ajax_node.py
+++ init/controllers/ajax_node.py
@@ -495,6 +495,7 @@ def ajax_node():
       TR(TD(T('type'), _style='font-style:italic'), TD(node['type'])),
       TR(TD(T('serial'), _style='font-style:italic'), TD(node['serial'])),
       TR(TD(T('enclosure'), _style='font-style:italic'), TD(node['enclosure'])),
+      TR(TD(T('security zone'), _style='font-style:italic'), TD(node['sec_zone'])),
       TR(TD(T('warranty end'), _style='font-style:italic'), TD(node['warranty_end'])),
       TR(TD(T('maintenance end'), _style='font-style:italic'), TD(node['maintenance_end'])),
       TR(TD(T('team responsible'), _style='font-style:italic'), TD(node['team_responsible'])),
