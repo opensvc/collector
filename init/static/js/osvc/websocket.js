5e0fe399eeb71dc52ff25c4882a23bcd4488a9c7
Christophe Varoqui <christophe.varoqui@opensvc.com>
2015-10-28 10:49:18 +0100

    Move websocket and compat code to dedicated js files

---
+++ init/static/js/osvc/websocket.js
@@ -0,0 +1,55 @@
+//
+// websockets
+//
+var wsh = {}
+var last_events = []
+
+function ws_duplicate_event(uid) {
+    if (last_events.indexOf(uid) >= 0) {
+        return true
+    }
+    last_events.push(uid)
+    if (last_events.length > 10) {
+        last_events = last_events.slice(0, 10)
+    }
+    return false
+}
+
+function ws_switch(e) {
+    var data = []
+    try {
+        data = eval('('+e.data+')')
+    } catch(ex) {
+        return
+    }
+    if (ws_duplicate_event(data['uuid'])) {
+        return
+    }
+    data = data['data']
+    for (i=0; i<data.length; i++) {
+        ws_switch_one(data[i])
+    }
+}
+
+function ws_switch_one(data) {
+    if (!("event" in data)) {
+        return
+    }
+    if (data["event"] == "action_q_change") {
+        action_queue_stats(data['data'])
+    }
+    for (key in wsh) {
+        if (!$("#wsswitch_"+key).prop('checked')) {
+            // websocket disabled for this table.
+            // just remember we have queued change.
+            osvc.tables[key].need_refresh = true
+            return
+        }
+        ws_action_switch = wsh[key]
+        ws_action_switch(data)
+    }
+}
+
+web2py_websocket("wss://"+window.location.hostname+"/realtime/generic", ws_switch)
+
+
