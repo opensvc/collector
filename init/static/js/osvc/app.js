c1888917944982a2c6e68dda7975bbf7d4e3aa0d
Christophe Varoqui <christophe.varoqui@opensvc.com>
2015-11-08 09:09:49 +0100

    Implement a load-from-menu error fallback to document.location

--- init/static/js/osvc/app.js
+++ init/static/js/osvc/app.js
@@ -16,6 +16,8 @@ function menu_entries_bind_click_to_load() {
     if (!href) {
       return
     }
+
+    // loadable co-functions ends with '_load'
     event.preventDefault()
     var _href
     if (href.indexOf("?") >= 0) {
@@ -24,18 +26,27 @@ function menu_entries_bind_click_to_load() {
     } else {
       _href = href + "_load"
     }
-    console.log("load", _href)
+
+    // update browser url and history
     history.pushState({}, "", href)
-    $(".layout").load(_href)
   
-    // purge tables not displayed anymore
-    for (tid in osvc.tables) {
-      if ($('#'+tid).length == 0) {
-        delete osvc.tables[tid]
-        if (tid in wsh) {
-          delete wsh[tid]
+    console.log("load", _href)
+    $(".layout").load(_href, {}, function (responseText, textStatus, req) {
+      if (textStatus == "error") {
+        // load error
+        console.log("fallback to location.href", _href)
+        document.location.replace(href)
+      } else {
+        // load success, purge tables not displayed anymore
+        for (tid in osvc.tables) {
+          if ($('#'+tid).length == 0) {
+            delete osvc.tables[tid]
+            if (tid in wsh) {
+              delete wsh[tid]
+            }
+          }
         }
       }
-    }
+    })
   })
 }
