b14f72bb9688ec64dfe8de2ae808777b988d9da3
Christophe Varoqui <christophe.varoqui@opensvc.com>
2015-11-17 19:42:55 +0100

    Menu fixes
    - reorder the node ruleset entry
    - remove comp entries in stats section
    - remove dup app_service entry
    - remove trailing whitespace
    - readd the 'r'efresh shortcut entry
    - reformat i18n dicts

--- init/static/js/osvc/menu/menu.js
+++ init/static/js/osvc/menu/menu.js
@@ -2,275 +2,260 @@
 
 function menu_search_key()
 {
-	var menu = 
+	var menu =
 	{
-		"views" : 
+		"views" :
 		[
-			{ 
+			{
 			  "title" : "dashboard",
 			  "class" : "alert16",
 			  "link" : "/init/dashboard/index" },
-			{ 
+			{
 			  "title" : "services",
 			  "class" : "svc",
 			  "link" : "/init/services/services" },
-			{ 
+			{
 			  "title" : "services_instances",
 			  "class" : "svc",
 			  "link" : "/init/default/svcmon" },
-			{ 
+			{
 			  "title" : "resources",
 			  "class" : "svc",
 			  "link" : "/init/resmon/resmon" },
-			{ 
+			{
 			  "title" : "app_info",
 			  "class" : "svc",
 			  "link" : "/init/appinfo/appinfo" },
-			{ 
+			{
 			  "title" : "nodes",
 			  "class" : "node16",
 			  "link" : "/init/nodes/nodes" },
-			{ 
+			{
 			  "title" : "tag_attachments",
 			  "class" : "fa-tags",
 			  "link" : "/init/tags/tagattach" },
-			{ 
+			{
 			  "title" : "actions",
 			  "class" : "actions",
 			  "link" : "/init/svcactions/svcactions" },
-			{ 
+			{
 			  "title" : "checks",
 			  "class" : "check16",
 			  "link" : "/init/checks/checks" },
-			{ 
+			{
 			  "title" : "packages",
 			  "class" : "pkg16",
 			  "link" : "/init/packages/packages" },
-			{ 
+			{
 			  "title" : "patches",
 			  "class" : "patch",
 			  "link" : "/init/patches/patches" },
-			{ 
+			{
 			  "title" : "networks",
 			  "class" : "net16",
 			  "link" : "/init/networks/networks" },
-			{ 
+			{
 			  "title" : "node_networks",
 			  "class" : "net16",
 			  "link" : "/init/nodenetworks/nodenetworks" },
-			{ 
+			{
 			  "title" : "node_san",
 			  "class" : "net16",
 			  "link" : "/init/nodesan/nodesan" },
-			{ 
+			{
 			  "title" : "san_switches",
 			  "class" : "net16",
 			  "link" : "/init/sanswitches/sanswitches" },
-			{ 
+			{
 			  "title" : "dns",
 			  "class" : "net16",
 			  "link" : "/init/dns/dns" },
-			{ 
+			{
 			  "title" : "saves",
 			  "class" : "save16",
 			  "link" : "/init/saves/saves" },
-			{ 
+			{
 			  "title" : "disks",
 			  "class" : "hd16",
 			  "link" : "/init/disks/disks" },
 		],
-		"compliance" : 
+		"compliance" :
 		[
-			{ 
+			{
 			  "title" : "statut",
 			  "class" : "check16",
 			  "link" : "/init/compliance/comp_status" },
-			{ 
+			{
 			  "title" : "log",
 			  "class" : "log16",
 			  "link" : "/init/compliance/comp_log" },
-			{ 
+			{
 			  "title" : "modulesets",
 			  "class" : "actions",
 			  "link" : "/init/compliance/comp_modules" },
-			{ 
+			{
 			  "title" : "rulesets",
 			  "class" : "comp16",
 			  "link" : "/init/compliance/comp_rules" },
-			{ 
+			{
 			  "title" : "designer",
 			  "class" : "wf16",
 			  "link" : "/init/compliance/comp_admin?obj_filter=opensvc" },
-			{ 
+			{
 			  "title" : "nrulesets",
 			  "class" : "node16",
 			  "link" : "/init/compliance/comp_rulesets_nodes_attachment" },
-			{ 
-			  "title" : "srulesets",
-			  "class" : "svc",
-			  "link" : "/init/compliance/comp_rulesets_services_attachment" },
-			{ 
-			  "title" : "service_modulesets",
-			  "class" : "svc",
-			  "link" : "/init/compliance/comp_modulesets_services" },
-			{ 
+			{
 			  "title" : "node_modulesets",
 			  "class" : "node16",
 			  "link" : "/init/compliance/comp_modulesets_nodes" },
+			{
+			  "title" : "srulesets",
+			  "class" : "svc",
+			  "link" : "/init/compliance/comp_rulesets_services_attachment" },
+			{
+			  "title" : "service_modulesets",
+			  "class" : "svc",
+			  "link" : "/init/compliance/comp_modulesets_services" },
 		],
-		"statistic" : 
+		"statistic" :
 		[
-			{ 
+			{
 			  "title" : "reports",
 			  "class" : "spark16",
 			  "link" : "/init/charts/reports" },
-			{ 
+			{
 			  "title" : "site",
 			  "class" : "spark16",
 			  "link" : "/init/stats/stats" },
-			{ 
+			{
 			  "title" : "compare",
 			  "class" : "spark16",
 			  "link" : "/init/stats/compare" },
-			{ 
+			{
 			  "title" : "os_lc",
 			  "class" : "spark16",
 			  "link" : "/init/lifecycle/lifecycle_os" },
-			{ 
+			{
 			  "title" : "availability",
 			  "class" : "avail16",
 			  "link" : "/init/svcmon_log/svcmon_log" },
-			{ 
-			  "title" : "n_rulesets",
-			  "class" : "node16",
-			  "link" : "/init/compliance/comp_rulesets_nodes_attachment" },
-			{ 
-			  "title" : "s_rulesets",
-			  "class" : "svc",
-			  "link" : "/init/compliance/comp_rulesets_services_attachment" },
-			{ 
-			  "title" : "s_modulesets",
-			  "class" : "svc",
-			  "link" : "/init/compliance/comp_modulesets_services" },
-			{ 
-			  "title" : "n_modulesets",
-			  "class" : "node16",
-			  "link" : "/init/compliance/comp_modulesets_nodes" },
 		],
-		"requests" : 
+		"requests" :
 		[
-			{ 
+			{
 			  "title" : "new_request",
 			  "class" : "wf16",
 			  "link" : "/init/forms/forms" },
-			{ 
+			{
 			  "title" : "a_t_m_t",
 			  "class" : "wf16",
 			  "link" : "/init/forms/workflows_assigned_to_me" },
-			{ 
+			{
 			  "title" : "p_t_a",
 			  "class" : "wf16",
 			  "link" : "/init/forms/workflows_pending_tiers_action" },
-			{ 
+			{
 			  "title" : "all_requests",
 			  "class" : "wf16",
 			  "link" : "/init/forms/workflows" },
 		],
-		"administration" : 
+		"administration" :
 		[
-			{ 
+			{
 			  "title" : "users",
 			  "class" : "guys16",
 			  "link" : "/init/users/users",
 			  "secure" : ["Manager"] },
-			{ 
+			{
 			  "title" : "log",
 			  "class" : "log16",
 			  "link" : "/init/log/log" },
-			{ 
+			{
 			  "title" : "o_setup",
 			  "class" : "obs16",
 			  "link" : "/init/obsolescence/obsolescence_config" },
-			{ 
+			{
 			  "title" : "application",
 			  "class" : "svc",
 			  "link" : "/init/apps/apps" },
-			{ 
+			{
 			  "title" : "drpplan",
 			  "class" : "drp16",
 			  "link" : "/init/drplan/drplan" },
-			{ 
+			{
 			  "title" : "batchs",
 			  "class" : "actions",
 			  "link" : "/init/batchs/batchs",
 			  "secure" : ["Manager"] },
-			{ 
+			{
 			  "title" : "billing",
 			  "class" : "bill16",
 			  "link" : "/init/billing/billing",
 			  "secure" : ["Manager"] },
-			{ 
-			  "title" : "app_services",
-			  "class" : "svc",
-			  "link" : "/init/apps/apps" },
-			{ 
+			{
 			  "title" : "prov",
 			  "class" : "prov",
 			  "link" : "/init/provisioning/prov_admin",
 			  "secure" : ["Manager","ProvManager"] },
-			{ 
+			{
 			  "title" : "filters",
 			  "class" : "filter16",
 			  "link" : "/init/compliance/comp_filters" },
-			{ 
+			{
 			  "title" : "forms",
 			  "class" : "wf16",
 			  "link" : "/init/forms/forms_admin" },
-			{ 
+			{
 			  "title" : "metrics",
 			  "class" : "spark16",
 			  "link" : "/init/charts/metrics_admin" },
-			{ 
+			{
 			  "title" : "charts",
 			  "class" : "spark16",
 			  "link" : "/init/charts/charts_admin" },
-			{ 
+			{
 			  "title" : "reports",
 			  "class" : "spark16",
 			  "link" : "/init/charts/reports_admin" },
-			{ 
+			{
 			  "title" : "tags",
 			  "class" : "fa-tags",
 			  "link" : "/init/tags/tags" },
 		],
 		"shortcuts" :
 		[
-			{ 
+			{
 			  "title" : "filter_sel",
 			  "class" : "",
 			  "label" : "f",
 			  "link" : "" },
-			{ 
+			{
 			  "title" : "link",
 			  "class" : "",
 			  "label" : "l",
 			  "link" : "" },
-			{ 
+			{
 			  "title" : "nav",
 			  "class" : "",
 			  "label" : "n",
 			  "link" : "" },
-			{ 
+			{
+			  "title" : "refresh",
+			  "class" : "",
+			  "label" : "r",
+			  "link" : "" },
+			{
 			  "title" : "search",
 			  "class" : "",
 			  "label" : "s",
 			  "link" : "" },
-			{ 
+			{
 			  "title" : "unfocus",
 			  "class" : "",
 			  "label" : "ESC",
 			  "link" : "" },
-			{ 
+			{
 			  "title" : "rest_api",
 			  "class" : "api",
 			  "label" : "",
@@ -284,7 +269,7 @@ function menu(divid) {
   var o = {}
   o.divid = divid
   o.div = $("#"+divid)
-  
+
   o.init = function init() {
     return menu_init(o)
   }
