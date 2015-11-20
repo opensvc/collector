fcd9390c4715e1dab8c9757b93578494bba2d9b2
Christophe Varoqui <christophe.varoqui@opensvc.com>
2015-11-20 09:52:54 +0100

    Remove the delete service tool from the service view
    This tool is now in the action menu and uses the rest api.

--- init/controllers/services.py
+++ init/controllers/services.py
@@ -47,67 +47,8 @@ class table_services(HtmlTable):
         self.ajax_col_values = 'ajax_services_col_values'
         self.span = ["svc_name"]
         self.keys = ["svc_name"]
-        self.additional_tools.append('svc_del')
+        self.events = ["services_change"]
 
-
-    def svc_del(self):
-        d = DIV(
-              A(
-                T("Delete service"),
-                _class='del16',
-                _onclick="""if (confirm("%(text)s")){%(s)s};"""%dict(
-                   s=self.ajax_submit(args=['svc_del']),
-                   text=T("Please confirm service deletion"),
-                ),
-              ),
-              _class='floatw',
-            )
-        return d
-
-@auth.requires_login()
-def svc_del(ids):
-    groups = user_groups()
-
-    q = db.services.svc_name.belongs(ids)
-    if 'Manager' not in groups:
-        # Manager can delete any svc
-        # A user can delete only services he is responsible of
-        l1 = db.apps.on(db.services.svc_app == db.apps.app)
-        l2 = db.apps_responsibles.on(db.apps.id == db.apps_responsibles.app_id)
-        l3 = db.auth_group.on(db.apps_responsibles.group_id == db.auth_group.id)
-        q &= (db.auth_group.role.belongs(groups)) | (db.auth_group.role==None)
-        ids = map(lambda x: x.id, db(q).select(db.services.id, left=(l1,l2,l3), cacheable=True))
-        q = db.services.id.belongs(ids)
-    rows = db(q).select(cacheable=True)
-    db(q).delete()
-    for r in rows:
-        _log('service.delete',
-             'deleted service %(u)s',
-              dict(u=r.svc_name),
-             svcname=r.svc_name)
-        purge_svc(r.svc_name)
-    if len(rows) > 0:
-        _websocket_send(event_msg({
-             'event': 'services_change',
-             'data': {'f': 'b'},
-            }))
-
-    svcnames = [r.svc_name for r in rows]
-    q = db.svcmon.mon_svcname.belongs(svcnames)
-    rows = db(q).select(cacheable=True)
-    db(q).delete()
-    for r in rows:
-        q = db.svcmon.id == r.id
-        _log('service.delete',
-             'deleted service instance %(u)s',
-              dict(u='@'.join((r.mon_svcname, r.mon_nodname))),
-             svcname=r.mon_svcname,
-             nodename=r.mon_nodname)
-    if len(rows) > 0:
-        _websocket_send(event_msg({
-             'event': 'svcmon_change',
-             'data': {'f': 'b'},
-            }))
 
 @auth.requires_login()
 def ajax_services_col_values():
@@ -126,16 +67,7 @@ def ajax_services_col_values():
 def ajax_services():
     t = table_services('services', 'ajax_services')
 
-    if len(request.args) == 1:
-        action = request.args[0]
-        try:
-            if action == 'svc_del':
-                svc_del(t.get_checked())
-        except ToolError, e:
-            t.flash = str(e)
-
     o = db.services.svc_name
-
     q = db.services.id > 0
     q = _where(q, 'services', domain_perms(), 'svc_name')
     q = apply_filters(q, None, db.services.svc_name)
@@ -156,7 +88,6 @@ def ajax_services():
         cols = t.get_visible_columns()
         t.object_list = db(q).select(*cols, orderby=o, limitby=limitby, cacheable=False)
         return t.table_lines_data(n, html=False)
-
 
 
 @auth.requires_login()
