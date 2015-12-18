92f317eda46432e2f6022393911f254d174ccd71
Christophe Varoqui <christophe.varoqui@opensvc.com>
2015-12-18 11:34:18 +0100

    Fix the action menu wrt the jquery data stored in table DOM cells
    Test the length of the jquery selector result before trying to
    fetch the jquery data.

--- init/static/js/osvc/tables/action_menu.js
+++ init/static/js/osvc/tables/action_menu.js
@@ -967,7 +967,11 @@ function table_action_menu_get_cols_data_clicked(t, e, scope, selector) {
   for (var i=0; i<selector.cols.length; i++) {
     var c = selector.cols[i]
     var s = t.column_selectors[c]
-    var val = $.data(line.find(s).first()[0], "v")
+    var cell = line.find(s).first()
+    if (cell.length == 0) {
+      continue
+    }
+    var val = $.data(cell[0], "v")
     if ((typeof val === "undefined") || (val=="") || (val == "empty")) {
       continue
     }
