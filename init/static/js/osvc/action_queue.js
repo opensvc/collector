c0688bdae931c1f94bda1c998a97b11e626ba117
Christophe Varoqui <christophe.varoqui@opensvc.com>
2016-03-01 16:08:41 +0100

    Fix action_queue widget vertical alignment when the q is empty
    And tab-indent the code.

--- init/static/js/osvc/action_queue.js
+++ init/static/js/osvc/action_queue.js
@@ -2,37 +2,41 @@
 // Action queue stats
 //
 function action_queue_stats(data) {
-    if (typeof data !== 'undefined') {
-      _action_queue_stats(data)
-      return
-    }
-    url = services_get_url() + "/init/rest/api/actions/stats"
-    $.getJSON(url, function(data){_action_queue_stats(data)})
+	if (typeof data !== 'undefined') {
+		_action_queue_stats(data)
+		return
+	}
+	url = services_get_url() + "/init/rest/api/actions/stats"
+	$.getJSON(url, function(data){_action_queue_stats(data)})
 }
 
 function _action_queue_stats(data) {
-    var s = ''
-    if ("data" in data)
-      data = data.data
-    if (!("queued" in data)) {
-      return
-    }
-    if (data["queued"] > 0) {
-      s += "<span class='boxed_small bgorange'>"+data["queued"]+'</span>'
-    }
-    if (data["ok"] > 0) {
-      s += "<span class='boxed_small bggreen'>"+data['ok']+'</span>'
-    }
-    if (data["ko"] > 0) {
-      s += "<span class='boxed_small bgred'>"+data['ko']+'</span>'
-    }
-    $(".header").find(".action_q_widget>a").html(s)
+	var s = ''
+	if ("data" in data) {
+		data = data.data
+	}
+	if (!("queued" in data)) {
+		return
+	}
+	if (data["queued"] > 0) {
+		s += "<span class='boxed_small bgorange'>"+data["queued"]+'</span>'
+	}
+	if (data["ok"] > 0) {
+		s += "<span class='boxed_small bggreen'>"+data['ok']+'</span>'
+	}
+	if (data["ko"] > 0) {
+		s += "<span class='boxed_small bgred'>"+data['ko']+'</span>'
+	}
+	if (s == '') {
+		s = "&nbsp;"
+	}
+	$(".header").find(".action_q_widget>a").html(s)
 }
 
 function action_queue_stats_wsh(data) {
-    if (data["event"] == "action_q_change") {
-        action_queue_stats(data['data'])
-    }
+	if (data["event"] == "action_q_change") {
+		action_queue_stats(data['data'])
+	}
 }
 
 wsh["action_queue_stats_wsh"] = action_queue_stats_wsh
