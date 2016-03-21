66dfc1ce375679d5f6340ba66812025e406b3e57
Christophe Varoqui <christophe.varoqui@opensvc.com>
2016-03-21 08:13:31 +0100

    Fix syntax in forms api

--- init/models/rest/api_forms.py
+++ init/models/rest/api_forms.py
@@ -158,8 +158,8 @@ class rest_get_forms(rest_get_table_handler):
 
     def handler(self, **vars):
         q = db.forms.id > 0
-        q &= db.forms.id == db.forms_team_publication.form_id)
-        q &= db.forms_team_publication.group_id.belongs(user_group_ids()
+        q &= db.forms.id == db.forms_team_publication.form_id
+        q &= db.forms_team_publication.group_id.belongs(user_group_ids())
         self.set_q(q)
         data = self.prepare_data(**vars)
         return data
