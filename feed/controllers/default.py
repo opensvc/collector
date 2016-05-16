adce62fd092d129c26f5615763c17e8c9c74d97d
Christophe Varoqui <christophe.varoqui@opensvc.com>
2016-05-16 16:52:32 +0200

    Fix actions dequeue
    The svcname info is important as used by the agent

--- feed/controllers/default.py
+++ feed/controllers/default.py
@@ -1325,7 +1325,8 @@ def rpc_collector_get_action_queue(nodename, auth):
     q = db.action_queue.node_id == node_id
     q &= db.action_queue.action_type == "pull"
     q &= db.action_queue.status == "W"
-    sql = db(q)._select()
+    l = db.services.on(db.action_queue.svc_id == db.services.svc_id)
+    sql = db(q)._select(db.action_queue.ALL, db.services.svcname, left=l)
     data = db.executesql(sql, as_dict=True)
     if len(data) > 0:
         db(q).update(status="Q")
