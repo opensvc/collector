2ddb1d16e2af7a34e644b5145854b1b8175681d7
Christophe Varoqui <christophe.varoqui@opensvc.com>
2016-11-15 15:54:49 +0100

    Fixes for the new action menu
    * fix background color for am displayed in tabs
    * reload am when the selection changes
    * open am upon checkbox check event
    * change the line highlight
    * restore cell1/cell2 differentiation on checked lines

--- init/static/js/osvc/tables/table.js
+++ init/static/js/osvc/tables/table.js
@@ -1247,6 +1247,7 @@ function table_init(opts) {
 		t.highlighed_checked_lines()
 	}
 	t.highlighed_checked_lines = function() {
+		table_action_menu(t, event)
 		t.div.find("input[name="+t.id+"_ck]").each(function(){
 			if ($(this).is(":checked")) {
 				$(this).parents("tr").first().addClass("tl_checked")
