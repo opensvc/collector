dfe791171764e66c905fe059e88c2c17529fc036
Sébastien Guihéneuf <sebastien.guiheneuf@opensvc.com>
2017-01-13 13:27:18 +0100

    Fix typo : "updateable"

--- init/models/rest/api_users.py
+++ init/models/rest/api_users.py
@@ -398,7 +398,7 @@ class rest_post_user(rest_post_handler):
                 del(vars["quota_org_group"])
 
         if "username" in vars and not login_form_username:
-            raise Exception(T("The 'username' property is updateable only with a collector setup for ldap authentication"))
+            raise Exception(T("The 'username' property is updatable only with a collector setup for ldap authentication"))
 
         db(q).update(**vars)
         l = []
