f96e561c72e1f52a548c5ef33818de84f124d89e
Sébastien Guihéneuf <sebastien.guiheneuf@opensvc.com>
2017-01-19 11:27:07 +0100

    Add "clicked selector decorator" for Applications

--- init/static/js/osvc/tables/action_menu.js
+++ init/static/js/osvc/tables/action_menu.js
@@ -802,6 +802,9 @@ function table_action_menu_init_data(t) {
 					"selector": ["clicked", "checked", "all"],
 					"foldable": true,
 					'title': 'action_menu.on_apps',
+					"clicked_decorator": function(e, data){
+						e.osvc_appname()
+					},
 					"class": "app16",
 					"table": ["apps"],
 					"cols": ["id"],
