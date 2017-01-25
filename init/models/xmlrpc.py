8fa563ecdc2fe77c2eaa55891f227ed7b668bf55
Christophe Varoqui <christophe.varoqui@opensvc.com>
2017-01-25 12:05:10 +0100

    Update init/models/xmlrpc.py from feed/model/xmlrpc.py
    The get_last_id logic was missing from the functions in init.

--- init/models/xmlrpc.py
+++ init/models/xmlrpc.py
@@ -1,17 +1,5 @@
-import datetime
-import xmlrpclib
-
-def chunker(lst, length):
-    i = -1
-    for i in range(len(lst) // length):
-        yield lst[i*length:(i+1)*length]
-    yield lst[(i+1)*length:]
-
 def value_wrap(a):
     return "%(a)s=values(%(a)s)"%dict(a=a)
-
-def var_wrap(a):
-    return "`%(a)s`"%dict(a=a)
 
 def quote_wrap(x):
     if isinstance(x, bool):
@@ -23,39 +11,58 @@ def quote_wrap(x):
         return str(x)
     elif isinstance(x, datetime.datetime):
         return "'%s'"%str(x)
-    elif isinstance(x, xmlrpclib.DateTime):
-        return "'%s'" % str(datetime.datetime.strptime(x.value, "%Y%m%dT%H:%M:%S"))
     elif isinstance(x, (str, unicode)):
         if len(x) == 0:
             return "''"
+        elif x.startswith("convert_tz("):
+            return x
         elif x[0] == "'" and x[-1] == "'":
             return x
         elif x[0] == '"' and x[-1] == '"':
             return x
-        elif x.startswith("unhex("):
-            return x
         else:
-            return "'%s'"%x.replace("'", "''")
+            return "'%s'"%x.replace("'", '"')
     elif x is None:
         return "NULL"
     raise Exception("quote_wrap: unhandled type %s"%str(x.__class__))
 
-def insert_multiline(table, vars, valsl):
+def insert_multiline(table, vars, valsl, node_id=None, get_last_id=False):
+    # convert agent time to server time
+    if node_id:
+        node = db(db.nodes.node_id==node_id).select(db.nodes.tz).first()
+    else:
+        node = None
+    if node:
+        tz = node.tz
+        for col in ("begin", "end"):
+            try:
+                idx = vars.index(col)
+                for i, v in enumerate(valsl):
+                    v[idx] = 'convert_tz("%s", "%s", @@time_zone)' % (v[idx].strip("'"), tz)
+                    valsl[i] = v
+            except:
+                pass
+
     value_wrap = lambda a: "%(a)s=values(%(a)s)"%dict(a=a)
     line_wrap = lambda x: "(%(x)s)"%dict(x=','.join(map(quote_wrap, x)))
-    vars = map(var_wrap, vars)
     upd = map(value_wrap, vars)
     lines = map(line_wrap, valsl)
-    sql="""insert delayed into %s (%s) values %s on duplicate key update %s""" % (table, ','.join(vars), ','.join(lines), ','.join(upd))
+    sql="""insert into %s (%s) values %s on duplicate key update %s""" % (table, ','.join(vars), ','.join(lines), ','.join(upd))
     db.executesql(sql)
+    if get_last_id:
+        i = db.executesql("SELECT LAST_INSERT_ID()")[0][0]
+        db.commit()
+        return i
     db.commit()
 
-def generic_insert(table, vars, vals):
+def generic_insert(table, vars, vals, node_id=None, get_last_id=False):
     if len(vals) == 0:
         return
     elif isinstance(vals[0], list):
-        insert_multiline(table, vars, vals)
+        i = insert_multiline(table, vars, vals, node_id=node_id, get_last_id=get_last_id)
     else:
-        insert_multiline(table, vars, [vals])
+        i = insert_multiline(table, vars, [vals], node_id=node_id, get_last_id=get_last_id)
     table_modified(table)
+    return i
+
 
