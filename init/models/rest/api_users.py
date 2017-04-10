6269450c96ec8d1584ff82799175e20da5d76b2a
Christophe Varoqui <christophe.varoqui@opensvc.com>
2017-04-10 13:47:53 +0200

    Fix /user/<id>/apps/{publications,responsibles} handlers
    Those used the logged-in user groups instead of the <id> user
    groups to determine the Manager privilege that influence the
    resultset computation.

--- init/models/rest/api_users.py
+++ init/models/rest/api_users.py
@@ -203,7 +203,7 @@ class rest_get_user_apps_responsible(rest_get_table_handler):
         )
 
     def handler(self, id, **vars):
-        if "Manager" in user_groups():
+        if "Manager" in user_groups(id):
             q = db.apps.id > 0
         else:
             q = allowed_user_ids_q()
@@ -234,7 +234,7 @@ class rest_get_user_apps_publication(rest_get_table_handler):
         )
 
     def handler(self, id, **vars):
-        if "Manager" in user_groups():
+        if "Manager" in user_groups(id):
             q = db.apps.id > 0
         else:
             q = allowed_user_ids_q()
