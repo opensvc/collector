opensvc/collector

commit b87972573ab451c5e06c68c5a7c1b7135fa1ec3a

Fix the post /apps handler handling of list data (bulk loads)

init/models/rest/api_apps.py
@@ -121,17 +121,15 @@ class rest_post_apps(rest_post_handler):
         if "id" in vars:
             app_id = vars["id"]
             del(vars["id"])
-            return rest_post_app(app_id, **vars)
+            return rest_post_app().handler(app_id, **vars)
+        if "app" in vars:
+            app = vars["app"]
+            del(vars["app"])
+            return rest_post_app().handler(app, **vars)
 
         check_privilege("AppManager")
         if len(vars) == 0 or "app" not in vars:
-            raise Exception("Insufficient data")
-        q = db.apps.id > 0
-        for v in vars:
-            q &= db.apps[v] == vars[v]
-        row = db(q).select().first()
-        if row is not None:
-            return rest_post_app().handler(row.id, **vars)
+            raise Exception("Insufficient data: %s" % str(vars))
         check_quota_app()
         response = db.apps.validate_and_insert(**vars)
         raise_on_error(response)

commit 1724542a4f84ad3d875eecd79609b2226382fa71

Fix post /apps
broken by b87972573ab451c5e06c68c5a7c1b7135fa1ec3a, make sure both
list and dict inputs work.

init/models/rest/api_apps.py
@@ -118,14 +118,18 @@ class rest_post_apps(rest_post_handler):
         )
 
     def handler(self, **vars):
+        _vars = {}
+        _vars.update(vars)
         if "id" in vars:
             app_id = vars["id"]
-            del(vars["id"])
-            return rest_post_app().handler(app_id, **vars)
-        if "app" in vars:
-            app = vars["app"]
-            del(vars["app"])
-            return rest_post_app().handler(app, **vars)
+            del(_vars["id"])
+        elif "app" in vars:
+            app_id = vars["app"]
+            del(_vars["app"])
+        else:
+            app_id = None
+        if app_id and lib_app_id(app_id):
+            return rest_post_app().handler(app_id, **_vars)
 
         check_privilege("AppManager")
         if len(vars) == 0 or "app" not in vars:
@@ -135,6 +139,7 @@ class rest_post_apps(rest_post_handler):
         raise_on_error(response)
         table_modified("apps")
         ws_send('apps_change')
+        q = db.apps.id == response
         row = db(q).select().first()
 
         db.apps_responsibles.insert(app_id=row.id, group_id=user_default_group_id())

init/models/version.py
@@ -1,1 +1,1 @@
-code_rev="9c7380c3ce102c89e9f6ea8c89ce81363f8b006d"
+code_rev="3a4db4a71cbb6c5835622a80b9ed75b73f108450"