opensvc/collector

commit b2541c3a77c766487ea6c62ee760a693e36f8dab

Add fr translation to select2

init/static/js/osvc/forms.js
@@ -1,3 +1,8 @@
+const userLocale =
+  navigator.languages && navigator.languages.length
+    ? navigator.languages[0]
+    : navigator.language;
+
 function convert_boolean(val) {
 	try {
 		if (String(val)[0].toLowerCase().match(/[1ty]/)) {
@@ -1205,6 +1210,7 @@ function form(divid, options) {
 				processResults: getProcessResultFunc(input, d),
 				transport: transport
 			},
+			language: userLocale,
 			allowClear: d.AllowClear,
 			placeholder: d.Placeholder || "Select a candidate",
 			minimumInputLength: d.SearchMinimumInputLength || 0,

init/models/version.py
@@ -1,1 +1,1 @@
-code_rev="d47bf0f82ea071b8c4fcc2b599fa1eb0f0803b2a"
+code_rev="fc856d0f5043d17dfcb8aa7e9b5b0c82feea8b06"

init/static/js/bootstrap-toggle-master/css/bootstrap2-toggle.min.css
@@ -1,28 +1,0 @@
-/*! ========================================================================
- * Bootstrap Toggle: bootstrap2-toggle.css v2.2.0
- * http://www.bootstraptoggle.com
- * ========================================================================
- * Copyright 2014 Min Hur, The New York Times Company
- * Licensed under MIT
- * ======================================================================== */
-label.checkbox .toggle,label.checkbox.inline .toggle{margin-left:-20px;margin-right:5px}
-.toggle{min-width:40px;height:20px;position:relative;overflow:hidden}
-.toggle input[type=checkbox]{display:none}
-.toggle-group{position:absolute;width:200%;top:0;bottom:0;left:0;transition:left .35s;-webkit-transition:left .35s;-moz-user-select:none;-webkit-user-select:none}
-.toggle.off .toggle-group{left:-100%}
-.toggle-on{position:absolute;top:0;bottom:0;left:0;right:50%;margin:0;border:0;border-radius:0}
-.toggle-off{position:absolute;top:0;bottom:0;left:50%;right:0;margin:0;border:0;border-radius:0}
-.toggle-handle{position:relative;margin:0 auto;padding-top:0;padding-bottom:0;height:100%;width:0;border-width:0 1px}
-.toggle-handle.btn-mini{top:-1px}
-.toggle.btn{min-width:30px}
-.toggle-on.btn{padding-right:24px}
-.toggle-off.btn{padding-left:24px}
-.toggle.btn-large{min-width:40px}
-.toggle-on.btn-large{padding-right:35px}
-.toggle-off.btn-large{padding-left:35px}
-.toggle.btn-small{min-width:25px}
-.toggle-on.btn-small{padding-right:20px}
-.toggle-off.btn-small{padding-left:20px}
-.toggle.btn-mini{min-width:20px}
-.toggle-on.btn-mini{padding-right:12px}
-.toggle-off.btn-mini{padding-left:12px}

init/static/js/select2@4.1.0-rc.0/i18n/fr.js
@@ -1,0 +1,3 @@
+/*! Select2 4.1.0-rc.0 | https://github.com/select2/select2/blob/master/LICENSE.md */
+
+!function(){if(jQuery&&jQuery.fn&&jQuery.fn.select2&&jQuery.fn.select2.amd)var e=jQuery.fn.select2.amd;e.define("select2/i18n/fr",[],function(){return{errorLoading:function(){return"Les résultats ne peuvent pas être chargés."},inputTooLong:function(e){var n=e.input.length-e.maximum;return"Supprimez "+n+" caractère"+(n>1?"s":"")},inputTooShort:function(e){var n=e.minimum-e.input.length;return"Saisissez au moins "+n+" caractère"+(n>1?"s":"")},loadingMore:function(){return"Chargement de résultats supplémentaires…"},maximumSelected:function(e){return"Vous pouvez seulement sélectionner "+e.maximum+" élément"+(e.maximum>1?"s":"")},noResults:function(){return"Aucun résultat trouvé"},searching:function(){return"Recherche en cours…"},removeAllItems:function(){return"Supprimer tous les éléments"},removeItem:function(){return"Supprimer l'élément"}}}),e.define,e.require}();

init/views/web2py_ajax.html
@@ -39,6 +39,7 @@ response.files.extend([
   URL(r=request,c='static',f='js/moment.min.js', vars={"v": code_rev}),
   URL(r=request,c='static',f='js/moment-timezone-with-data-1970-2030.min.js', vars={"v": code_rev}),
   URL(r=request,c='static',f='js/select2/select2.min.js', vars={"v": code_rev}),
+  URL(r=request,c='static',f='js/select2/i18n/fr.js', vars={"v": code_rev}),
   URL(r=request,c='static',f='css/custom.css', vars={"v": code_rev}),
   URL(r=request,c='static',f='js/require.js', vars={"v": code_rev}),
   URL(r=request,c='static',f='js/osvc/md5.js', vars={"v": code_rev}),

commit bdb4efd35dd6b508105d095bb66fa1e74d2c5db0

Fix the multiple=yes select2, broken by b2541c3a77c766487ea6c62ee760a693e36f8dab
Use a "initialized" property instead of hinting from a visible parent to
decide we need to run select2().

init/static/js/osvc/forms.js
@@ -886,9 +886,11 @@ function form(divid, options) {
 		o.area.find("select[data-select2-id]:visible,input[data-select2-id]:visible").each(function(){
 			let w = $(this)
 			let options = $.data(this, "s2options");
+			let initialized = w.prop("initialized")
 			od = o.get_option_data(w.parents("td"))
-			if (typeof(od) === "undefined") {
+			if (!initialized) {
 				w.select2(options)
+				w.prop("initialized", true)
 			}
 		})
 	}