b9f2cc66d47041995bb95d25485a9dd5119f0407
Cyril Galibern <cyril.galibern@opensvc.com>
2024-03-04 16:39:54 +0100

    refactor fix 'multiple services found matching the service name' after join new cluster

--- feed/models/scheduler.py
+++ feed/models/scheduler.py
@@ -302,9 +302,9 @@ def _update_service(svcname, auth):
         if cluster_id:
             # take time to verify unexpected node cluster id value, it may be outdated after daemon join new cluster.
             # Outdated node cluster_id values may lead to unexpected services creation during next node_svc_id call.
-            db_cluster_id = db(db.nodes.node_id == node_id).select(db.nodes.cluster_id).first()
-            if db_cluster_id and db_cluster_id != cluster_id:
-                db(db.nodes.node_id == node_id).update(cluster_id=cluster_id)
+            row = db(db.nodes.node_id == node_id).select(db.nodes.cluster_id).first()
+            if row and row.cluster_id != cluster_id:
+                db(db.nodes.node_id == node_id).update(cluster_id=row.cluster_id)
                 db.commit()
     svcname = h["svcname"].strip("'")
     svc_id = node_svc_id(node_id, svcname)
